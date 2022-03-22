3b7c1d0ea8dfc5b8913f122fe42b137570f66a33
Victor Santos <83377611+victorsantosg@users.noreply.github.com>
2022-03-22 18:26:11 -0300

    Atl

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 function Home(){
     return(
         <>
-            <h3>HOME</h3>
+           <navbar id="navbar">
+  <div className="navbar-container">
+    <img src="./img/logo.png" alt="" className="logo" />
+    <ul className="navbar-items">
+      <li className="navbar-item">Premium</li>
+      <li className="navbar-item">Ajuda</li>
+      <li className="navbar-item">Baixar</li>
+      <li className="navbar-item">Inscrever-se</li>
+      <li className="navbar-item">Entrar</li>
+    </ul>
+  </div>
+</navbar>
+
         
         </>
     );
